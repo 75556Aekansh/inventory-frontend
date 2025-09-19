39e9ecbb048255f0d5d36c298c79d28500a1df12
AEKANSH SINGH <127930584+75556Aekansh@users.noreply.github.com>
2025-09-19 12:30:12 +0530

    Update Login.js

--- src/pages/UserLogin/Login.js
+++ src/pages/UserLogin/Login.js
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 import './Login.css';
 import React from "react";
 
-
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -21,12 +20,32 @@ const Login = () => {
     }
   };
 
+  const useMasterUser = () => {
+    setUsername("admin");
+    setPassword("admin123");
+  };
+
   return (
     <div className="login-container">
-      <h2>Login</h2>
-      <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
-      <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <h2>User Login</h2>
+
+      <input
+        placeholder="Username"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
       <button onClick={handleLogin}>Login</button>
+      <div className="default-creds">
+        <button onClick={useMasterUser} type="button">
+          Use Master User
+        </button>
+      </div>
     </div>
   );
 };
