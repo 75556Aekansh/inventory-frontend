a36086d951f59a8b2431544af69049e4f127cb15
AEKANSH SINGH <127930584+75556Aekansh@users.noreply.github.com>
2025-09-19 11:05:07 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, 
+  baseURL: "https://inventory-backend.onrender.com/api", 
 });
 
 // Add JWT token to headers
