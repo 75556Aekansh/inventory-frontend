bc7b3ba079fb50f18d676f7d4017439f1e75875a
AEKANSH SINGH <127930584+75556Aekansh@users.noreply.github.com>
2025-09-19 10:23:36 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api", // backend base URL
+  baseURL: process.env.REACT_APP_API_URL, 
 });
 
 // Add JWT token to headers
